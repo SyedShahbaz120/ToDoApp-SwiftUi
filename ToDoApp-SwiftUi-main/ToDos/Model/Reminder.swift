66d5af0a420c48de293b901ea808dda4f96119c7
RENANYANG <ryanren991128@gmail.com>
2024-06-23 15:37:24 -0400

    #10 Complete task status
    Closes #10

--- ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
+++ ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
@@ -3,12 +3,16 @@ import Foundation
 import SwiftData
 
 @Model
+
+//this aloows us to create a reminder object
 final class Reminder {
     var name: String
     var isCompleted = false
-    
-    init(name: String, isCompleted: Bool = false) {
+
+     init(name: String, isCompleted: Bool = false) {
         self.name = name
         self.isCompleted = isCompleted
     }
+    
+   
 }
