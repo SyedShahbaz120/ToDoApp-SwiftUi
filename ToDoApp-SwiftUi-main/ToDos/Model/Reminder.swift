6f5496179b9f28253df7b0433228aa979b18c3dd
Urvish Patel <u_patel210812@fanshaweonline.ca>
2024-07-21 22:22:11 -0400

    #8 Urgent Task Implementation Start - add InUrgent Property.

--- ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
+++ ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
@@ -1,18 +1,36 @@
-//Created By S2G8 
+////Created By S2G8 
+//import Foundation
+//import SwiftData
+//
+//@Model
+//
+////this aloows us to create a reminder object
+//final class Reminder {
+//    var name: String
+//    var isCompleted = false
+//
+//     init(name: String, isCompleted: Bool = false) {
+//        self.name = name
+//        self.isCompleted = isCompleted
+//    }
+//    
+//   
+//}
+
+// Reminder.swift
+// Created By S2G8
 import Foundation
 import SwiftData
 
 @Model
-
-//this aloows us to create a reminder object
 final class Reminder {
     var name: String
     var isCompleted = false
+    var isUrgent = false // Add this property
 
-     init(name: String, isCompleted: Bool = false) {
+    init(name: String, isCompleted: Bool = false, isUrgent: Bool = false) {
         self.name = name
         self.isCompleted = isCompleted
+        self.isUrgent = isUrgent
     }
-    
-   
 }
