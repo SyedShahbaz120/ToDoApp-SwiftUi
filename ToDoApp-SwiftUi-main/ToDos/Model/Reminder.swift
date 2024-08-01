62fc911f461f25759fc19d5b93e7f152bb2389e2
SyedShahbaz120 <syedshahbaz120@gmail.com>
2024-08-01 15:51:18 -0400

    #9 Starts implementing App Preferences like Enabling Dark Mode

--- ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
+++ ToDoApp-SwiftUi-main/ToDos/Model/Reminder.swift
@@ -1,23 +1,4 @@
-////Created By S2G8 
-//import Foundation
-//import SwiftData
-//
-//@Model
-//
-////this aloows us to create a reminder object
-//final class Reminder {
-//    var name: String
-//    var isCompleted = false
-//
-//     init(name: String, isCompleted: Bool = false) {
-//        self.name = name
-//        self.isCompleted = isCompleted
-//    }
-//    
-//   
-//}
 
-// Reminder.swift
 // Created By S2G8
 import Foundation
 import SwiftData
