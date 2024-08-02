837e455d3095b322665990a058343d907eef979c
RENANYANG <ryanren991128@gmail.com>
2024-08-01 20:24:27 -0400

    #7 Complete timer feature
    Finished timer and makes homeview looks cleaner #7

---
+++ ToDoApp-SwiftUi-main/ToDos/View/Home/TimerManager.swift
@@ -0,0 +1,35 @@
+//
+//  TimerManager.swift
+//  ToDos
+//
+//  Created by RENANYANG on 2024-08-01.
+//
+//this is to manage the timer,so user can set an countdown timer for the task
+import SwiftUI
+import Combine
+
+class TimerManager: ObservableObject {
+    @Published var remainingTime: Int = 0
+    @Published var showAlert: Bool = false
+    
+    private var timer: Timer?
+    
+    func startTimer(hours: Int, minutes: Int, seconds: Int) {
+        let totalSeconds = hours * 3600 + minutes * 60 + seconds
+        remainingTime = totalSeconds
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            if self.remainingTime > 0 {
+                self.remainingTime -= 1
+            } else {
+                self.timer?.invalidate()
+                self.showAlert = true
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        remainingTime = 0
+    }
+}
